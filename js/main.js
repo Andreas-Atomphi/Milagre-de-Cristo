bf01b809946ecf3f2e42d2f7975083da705882dc
Andreas-Atomphi <andreasfariaen3@gmail.com>
2021-03-05 18:37:43 -0300

    [FEAT] Site mais estilizado e adicionado um novo mapa;
    Adicionada opção de ir ao facebook da igreja;
    Algumas funções foram melhoradas;
    A Navbar muda a seleção de acordo com a scrollTop da página;

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var embeds = {
+    igrejas: [
+        {
+            titulo: "Sede(Principal)",
+            next: "Filial",
+            src:
+                "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d160916.97641584076!2d-46.555473940451314!3d-23.460854913456735!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef58a1b0d9169%3A0xc95b8da333866511!2sIgreja%20Petencostal%20Milagre%20De%20Cristo!5e0!3m2!1spt-BR!2sbr!4v1614977175725!5m2!1spt-BR!2sbr",
+        },
+        {
+            titulo: "Filial",
+            next: "Sede",
+            src:
+                "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d914.8604427136371!2d-46.57105651193203!3d-23.480592299040516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef58db44d0557%3A0xc20ddbdf9c5264a3!2sR.%20%C3%81lvaro%20dos%20Santos%2C%20193%20-%20Vila%20Medeiros%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2002227-210!5e0!3m2!1spt-BR!2sbr!4v1614977234839!5m2!1spt-BR!2sbr",
+        },
+    ],
+};
+
+var selector = 0;
